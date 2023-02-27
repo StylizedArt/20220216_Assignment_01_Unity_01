1d1098563356459b9b6400f1dcefc1641e32fd65
GitMeade <jaelynmeade@gmail.com>
2023-02-28 08:58:05 +1100

    Player movement animations added
    - Movement
    - Jump

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     [Tooltip("Movement Values")]
-    [SerializeField] float movementSpeed, rotationSpeed;
+    [SerializeField] float movementSpeed, rotationSpeed, gravityForce, jumpForce;
 
     // Components
     CharacterController cc;
@@ -15,6 +15,10 @@ public class CharacterMovement : MonoBehaviour
     Camera cam;
 
     public Transform target;
+
+    // Gravity and jump
+    Vector3 playerVelocity;
+    public bool groundedPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -28,6 +32,13 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        groundedPlayer = cc.isGrounded;
+        if(groundedPlayer && playerVelocity.y < 0 )
+        {
+            if (anim.GetBool("Jump")) anim.SetBool("Jump", false);
+            playerVelocity.y = 0;
+        }
+
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
@@ -35,9 +46,39 @@ public class CharacterMovement : MonoBehaviour
         Vector3 camh = cam.transform.right;
         Vector3 camv = Vector3.Cross(camh, Vector3.up);
 
-        movementDirection = camh * h + camv * v;
-        movementDirection.Normalize();
 
-        cc.Move(movementDirection * movementSpeed * Time.deltaTime);
+        if(h != 0 || v != 0)
+        {
+            movementDirection = camh * h + camv * v;
+            movementDirection.Normalize();
+            cc.Move(movementDirection * movementSpeed * Time.deltaTime);
+
+            anim.SetBool("HasInput", true);
+        }
+        else
+        {
+            anim.SetBool("HasInput", false);
+        }
+
+        Quaternion desiredDirection = Quaternion.LookRotation(movementDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredDirection, rotationSpeed);
+
+        Vector3 animationVector = anim.transform.InverseTransformDirection(cc.velocity);
+
+        anim.SetFloat("HorizontalSpeed", animationVector.x);
+        anim.SetFloat("VerticalSpeed", animationVector.z);
+
+        ProcessGravity();
+    }
+
+    public void ProcessGravity()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && groundedPlayer)
+        {
+            anim.SetBool("Jump", true);
+            playerVelocity.y += Mathf.Sqrt(jumpForce * -3.0f * gravityForce);
+        }
+        playerVelocity.y += gravityForce * Time.deltaTime;
+        cc.Move(playerVelocity * Time.deltaTime);
     }
 }
